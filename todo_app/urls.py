193984c88b7b91ee43eb5f332aaf0b4c88eb8e42
yaerankim <yaerankim000812@gmail.com>
2023-02-07 18:07:20 +0900

    리팩토링: extra-patterns를 사용하여 중복 코드를 줄이도록 todo_app/urls.py 수정

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
+
+# 2) extra-patterns를 사용하여 URLconf에서의 중복 제거하는 방식
+extra_patterns = [
+    path('add/', views.ItemCreate.as_view(), name="item-add"),
+    path('<int:pk>/', views.ItemUpdate.as_view(), name="item-update"),
+    path('<int:pk>/delete/', views.ItemDelete.as_view(), name="item-delete"),
+]
 
 urlpatterns = [
 	path("", views.ListListView.as_view(), name="index"),
@@ -10,19 +17,23 @@ urlpatterns = [
         "list/<int:pk>/delete/", views.ListDelete.as_view(), name="list-delete"
     ),
     # CRUD patterns for ToDoItems
-    path(
-        "list/<int:list_id>/item/add/",
-        views.ItemCreate.as_view(),
-        name="item-add",
-    ),
-    path(
-        "list/<int:list_id>/item/<int:pk>/",
-        views.ItemUpdate.as_view(),
-        name="item-update",
-    ),
-    path(
-        "list/<int:list_id>/item/<int:pk>/delete/",
-        views.ItemDelete.as_view(),
-        name="item-delete",
-    ),
+    # 1) 하나의 URL pattern을 사용하는 방식
+    # path(
+    #     "list/<int:list_id>/item/add/",
+    #     views.ItemCreate.as_view(),
+    #     name="item-add",
+    # ),
+    # path(
+    #     "list/<int:list_id>/item/<int:pk>/",
+    #     views.ItemUpdate.as_view(),
+    #     name="item-update",
+    # ),
+    # path(
+    #     "list/<int:list_id>/item/<int:pk>/delete/",
+    #     views.ItemDelete.as_view(),
+    #     name="item-delete",
+    # ),
+    ##############################################################
+    # 2) extra-patterns를 사용하여 URLconf에서의 중복 제거하는 방식
+    path('list/<int:list_id>/item/', include(extra_patterns)),
 ]
